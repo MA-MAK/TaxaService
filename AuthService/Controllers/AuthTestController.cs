04dd5cacc90e2860549b55f992f68bfd48481843
morten-lund <morten2608@gmail.com>
2023-11-15 10:56:58 +0100

    AuthenticationService added and working

---
+++ AuthService/Controllers/AuthTestController.cs
@@ -0,0 +1,33 @@
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+[ApiController]
+[Route("[controller]")]
+public class AuthTestController : ControllerBase
+{
+
+    private readonly ILogger<AuthTestController> _logger;
+    private readonly IConfiguration _config;
+
+    public AuthTestController(ILogger<AuthTestController> logger, IConfiguration config)
+    {
+        _config = config;
+        _logger = logger;
+    }
+
+    [Authorize]
+    [HttpGet("test")]
+    public async Task<IActionResult> GetTest()
+    {
+        return Ok("You're authorized");
+    }
+
+}
---
+++ AuthService/Models/LoginModel.cs
@@ -0,0 +1,10 @@
+using System.Security.Claims;
+public class LoginModel
+{
+    public string? Username { get; set; }
+    public string? Password { get; set; }
+ 
+
+}
+
+
